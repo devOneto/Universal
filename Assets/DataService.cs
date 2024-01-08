1aef125239a80d7c556bfcfd15c49025ffe21cc9
Neto(Joao Araujo) <bipnerds@gmail.com>
2024-01-08 14:13:45 -0300

    - First Commit; - Adding POC saving level with positioned objects.

---
+++ Assets/DataService.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class DataService : IDataService
+{
+    public T LoadData<T>(string RelativePath)
+    {
+        try
+        {
+            using ( FileStream stream = new FileStream(RelativePath, FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string fileText = reader.ReadToEnd();
+                    T resultObject = JsonConvert.DeserializeObject<T>(fileText);
+                    return resultObject;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Não foi possível carregar o save. Error: " + e.Message ); 
+            return JsonConvert.DeserializeObject<T>("");
+        }
+    }
+
+    public bool SaveData<T>(string RelativePath, T Data)
+    {
+        string path = RelativePath; //TODO: definir uma pasta para salvar
+        string serializedResult = "";
+
+        try
+        {
+            if( File.Exists(path) ) File.Delete(path);
+            
+            if ( Data != null) serializedResult = JsonConvert.SerializeObject(Data);
+
+            using ( FileStream stream = new FileStream(RelativePath, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    writer.Write(serializedResult);
+                }
+            }
+
+            return true;
+        
+        } 
+        catch (Exception e) 
+        { 
+            Debug.LogError("Não foi possível salvar. Error: " + e.Message ); 
+            return false;    
+        }
+    }
+}
