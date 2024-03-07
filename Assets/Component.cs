461cb9fbd440b960df27648d064cbebf7d130269
Neto(Joao Araujo) <13579770+devOneto@users.noreply.github.com>
2024-03-07 19:05:08 -0300

    - Loading Components

--- Assets/Component.cs
+++ Assets/Component.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Component
 {
     public string Name { get; set; } = "";
-    public string MeshName { get; set; } = "";
+    public string MeshPath { get; set; } = "";
     public Position RelativePosition { get; set; }
     public ICollection<Component> Children { get; set; }
 
