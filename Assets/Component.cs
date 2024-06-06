493a95a24ce81299c66a74db62ce89a6aa5e368e
Neto(Joao Araujo) <13579770+devOneto@users.noreply.github.com>
2024-06-06 09:35:42 -0300

    - Setting up rotation (WIP)

--- Assets/Component.cs
+++ Assets/Component.cs
@@ -7,6 +7,7 @@ public class Component
     public string Name { get; set; } = "";
     public string MeshPath { get; set; } = "";
     public Position RelativePosition { get; set; } = new Position();
+    public Rotation Rotation { get; set; } = new Rotation();
     public IList<Component?> Components { get; set; } = new List<Component>();
 
 }
